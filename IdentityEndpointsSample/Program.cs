fa8ef1ff72dee69002023c0856109bed8899c834
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2023-08-16 19:46:37 +1200

    Use of explicit type.

--- IdentityEndpointsSample/Program.cs
+++ IdentityEndpointsSample/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
 builder.Services.AddAuthorizationBuilder();
@@ -18,7 +18,7 @@ builder.Services.AddIdentityCore<MyUser>()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Adds /register, /login and /refresh endpoints
 app.MapIdentityApi<MyUser>();
